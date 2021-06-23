266a604b22ceae2d03111dd4e894b71887ba377e
peppe-p <ombrachiara96@gmail.com>
2021-06-23 16:30:12 +0200

    snack1 create foreach for match

--- SNACK_1.php
+++ SNACK_1.php
@@ -4,42 +4,50 @@ Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squ
 di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
 Olimpia Milano - Cantù | 55-60
  -->
- <?php 
+<?php 
  $partite = [
      [
          'casa' => "Milano",
          'ospite' => "Roma",
-         "punti" => "40-50"
+         "punti" => "40 - 50"
      ],
      [
          'casa' => "Palermo",
          'ospite' => "Catania",
-         "punti" => "75-40"
+         "punti" => "75 - 40"
      ],
      [
          'casa' => "Napoli",
          'ospite' => "Palermo",
-         "punti" => "30-50"
+         "punti" => "30 - 50"
      ],
      [
          'casa' => "Pisa",
          'ospite' => "Roma",
-         "punti" => "50-45"
+         "punti" => "50 - 45"
      ],
     ];
-
-    var_dump($partite);
  ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SNACK 1</title>
 </head>
+
 <body>
-    
+    <?php foreach ($partite as $partita){
+        ?>
+    <ol>
+        <li><?php $partita["casa"] . ' - '. $partita["ospite"] ?><br> <?php $partita["punti"] ?></li>
+    </ol>
+    <?php
+    }
+    ?>
 </body>
+
 </html>
