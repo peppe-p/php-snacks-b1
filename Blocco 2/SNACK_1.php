39a49432dd6ab8d84217def5d6d183b713513a21
peppe-p <ombrachiara96@gmail.com>
2021-06-23 12:05:32 +0200

    complete snack 1 blocco 2

---
+++ Blocco 2/SNACK_1.php
@@ -0,0 +1,68 @@
+<?php
+
+$ads = [
+    [
+        'image_path' => 'https://conversionsciences.com/wp-content/uploads/2019/04/example-of-emotional-logical-appeal-on-persuasive-copy.jpg',
+        'link' => 'https://www.google.com',
+        'is_active' => true,
+    ],
+    [
+        'image_path' => 'https://previews.123rf.com/images/hstrongart/hstrongart1612/hstrongart161200121/68060446-anuncios-de-cepillos-de-dientes-el%C3%A9ctricos-modo-diferente-de-este-producto-con-blanco-modelo-de-diente.jpg',
+        'link' => 'https://www.facebook.com',
+        'is_active' => true,
+    ],
+    [
+        'image_path' => 'https://thumbs.dreamstime.com/z/back-to-school-sale-background-chalkboard-sale-percentages-marketing-poster-color-pencils-96751666.jpg',
+        'link' => 'https://codepen.io',
+        'is_active' => false,
+    ],
+    [
+        'image_path' => 'https://foodsecurityfoodjustice.files.wordpress.com/2016/11/beyond-the-omlette-1.jpg',
+        'link' => 'https://laravel.com',
+        'is_active' => false,
+    ],
+    [
+        'image_path' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRniLfGdFLxVOUoEwYC9WeRIrfZInB74_80IV8yMnANx3HTZYoQ',
+        'link' => 'https://www.php.net',
+        'is_active' => true,
+    ],
+
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SNACK 1 - B2</title>
+
+    <style>
+        section {
+            display: flex;
+        }
+
+        img {
+            height: 300px;
+        }
+    </style>
+</head>
+<body>
+    <section>
+    <?php foreach ($ads as $ad){
+        if ($ad["is_active"] == true) {
+            ?>
+            <div class="ad">
+            <a href="<?php echo $ad["link"] ?>">
+            <img src="<?php echo $ad["image_path"] ?>" alt="">
+            </a>
+            </div>
+            <?php
+        }
+    };
+    ?>
+</section>
+</body>
+</html>
